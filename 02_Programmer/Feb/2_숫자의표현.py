1203a88e1a214c9028d7af7b96e51c9df5f5f155
Taekyun Kim <61183115+teqn99@users.noreply.github.com>
2021-02-02 12:03:08 +0900

    Update 2_숫자의표현.py

--- 02_Programmer/Feb/2_숫자의표현.py
+++ 02_Programmer/Feb/2_숫자의표현.py
@@ -1,5 +1,7 @@
 def solution(n):
     m, answer, ans = n // 2, 1, 0    # answer: n 자기자신도 결과에 포함
+    # n을 반으로 나눈 m을 사용하면서 for문 반복횟수 줄이기
+    # (n을 반으로 나눈 값 이후로 연속된 자연수들의 합은 n보다 크다.)
 
     for i in range(1, m + 1):
         ans = 0
